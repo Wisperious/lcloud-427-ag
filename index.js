b2f682e7ff46515dabe1663b267e107664d126de
Paweł <pawelwozniakx@gmail.com>
2019-04-16 20:43:16 +0200

    done upload local file

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 
 const AWS = require('aws-sdk');
 const uuid = require('uuid');
+const fs = require('fs');
 
 const bucketName = 'lcloud-427-ag';
 
-var bucketPromise = new AWS.S3({apiVersion: '2006-03-01'}).listObjectsV2({Bucket: bucketName}).promise();
+var listPromise = new AWS.S3({apiVersion: '2006-03-01'}).listObjectsV2({Bucket: bucketName}).promise();
 
 
-bucketPromise.then(
+listPromise.then(
     function (data) {
         console.log(data.Contents.forEach(file => console.log(file.Key)));
     }
@@ -16,3 +17,21 @@ bucketPromise.then(
         console.error(err)
     }
 );
+
+var readStream = fs.readFile('myfile.txt', function (err, data) {
+
+    var base64data = new Buffer.from(data);
+
+    var uploadPromise = new AWS.S3({apiVersion: '2006-03-01'}).upload({Bucket: bucketName, Key: 'data.txt', Body: base64data}).promise();
+
+    uploadPromise.then(
+        function (data) {
+            console.log(data);
+        }
+    ).catch(
+        function (err) {
+            console.error(err);
+        }
+    );
+});
+
